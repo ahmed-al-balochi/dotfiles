778d34a5da1017d2a4d975d78369e6df2f6f5a33
a7mad98ksa <a7mad98.ksa@gmail.com>
2020-08-30 02:14:46 +0500

    added blur picom

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -39,7 +39,9 @@ run pamac-tray &
 run xfce4-power-manager &
 #numlockx on &
 blueberry-tray &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
+redshift &
+picom --experimental-backends &
+#picom --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 
