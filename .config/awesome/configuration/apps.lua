c0a1bc2950edc21ce6cf35f24b8369511e429648
Ahmed Al Balochi <a7mad98.work@gmail.com>
2021-12-04 11:13:31 +0500

    minor changes to awesome WM

--- .config/awesome/configuration/apps.lua
+++ .config/awesome/configuration/apps.lua
@@ -49,6 +49,7 @@ return {
     'nitrogen --restore',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
-    '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
+    '~/.config/awesome/configuration/awspawn', -- Spawn "dirty" apps that can linger between sessions
+    '/usr/lib/nvidia-prime-applet/nvidia-prime'
   }
 }
