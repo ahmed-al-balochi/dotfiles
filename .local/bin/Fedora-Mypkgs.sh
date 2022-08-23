1d9414f77e8045cd3766d534316c4306232330bf
Ahmed Al Balochi <a7mad98.work@gmail.com>
2022-08-23 08:01:59 +0500

    minor updates to dotfiles

--- .local/bin/Fedora-Mypkgs.sh
+++ .local/bin/Fedora-Mypkgs.sh
@@ -13,6 +13,9 @@ sudo dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+echo "Adding flatpak flathub... \n"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 else
    printf " Canceled Setup"
 fi
@@ -26,7 +29,7 @@ if [ $in = $y ]
    printf " Downloading PKGs\n"
 sudo dnf install rofi awesome kitty neovim materia-gtk-theme lxappearance nitrogen volumeicon network-manager-applet redshift flameshot vifm parcellite blueman xfce4-notifyd xfce4-power-manager pavucontrol cmatrix htop speedtest-cli zsh-syntax-highlighting autojump zsh-autosuggestions papirus-icon-theme playerctl ibus jetbrains-mono-fonts-all.noarch qbittorrent timeshift polkit-gnome 
 
-echo "Installing Codium and Brave"
+echo "Installing Codium, Obsidian and Brave"
 ## Brave
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
@@ -34,8 +37,8 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 ## Codium
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install codium brave-browser
-
+sudo dnf install codium brave-browser -y
+flatpak install flathub md.obsidian.Obsidian -y
 # Not available in apt ==> picom exa auto-cpufreq optimus-manager acpi_call bbswitch chsh ttf-joypixels ttf-all-the-icons
 #iBus is for keyboad layout switching
 else
